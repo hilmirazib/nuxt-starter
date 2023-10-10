6ee510debbc8e8f5d148611f4838a7b3cb92dbc5
Hilmi Razib Yusuf <hilmiyusuf197@gmail.com>
2023-10-10 14:57:45 +0700

    setup nuxt 3 with tailwindcss and sass

---
+++ pages/events/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>event: {{ $route.params.id }}</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<style scoped></style>
---
+++ pages/events/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>events</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
